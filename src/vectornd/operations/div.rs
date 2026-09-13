3d6cb8ec4a704e6f3a3378451ded08e04e859cb3
Enumir <zadorin.grigoriy@gmail.com>
2026-09-13 18:55:33 +0300

    Сделал то же и для скалярного деления. Теперь работает с &mut

--- src/vectornd/operations/div.rs
+++ src/vectornd/operations/div.rs
@@ -1,27 +1,42 @@
 use crate::VectorND;
 use std::ops::{Div, DivAssign};
+use std::borrow::Borrow;
 
-//Образующие
-impl<const N: usize> Div<f64> for &VectorND<N> {
-    type Output = VectorND<N>;
+impl<const N: usize> VectorND<N> {
     #[inline]
-    fn div(self, rhs: f64) -> Self::Output {
-        self.clone() * (1.0 / rhs)
+    fn generic_alloc_div(a: impl Borrow<Self>, b: f64) -> Self {
+        let a = a.borrow();
+        let factor = 1.0/b;
+        Self::from_fn(|i| a[i] * factor)
     }
 }
 
 impl<const N: usize> DivAssign<f64> for VectorND<N> {
     #[inline]
     fn div_assign(&mut self, rhs: f64) {
-        *self *= 1.0 / rhs
+        let factor = 1.0/rhs;
+        for s in self.iter_mut() {
+            *s *= factor;
+        }
     }
 }
 
-//Образованные
-impl<const N: usize> Div<f64> for VectorND<N> {
-    type Output = Self;
-    #[inline]
-    fn div(mut self, rhs: f64) -> Self::Output {
-        self /= rhs; self
-    }
+macro_rules! impl_div {
+    ($lhs:ty, f64, $logic_closure:expr) => {
+        impl<const N: usize> Div<f64> for $lhs {
+            type Output = VectorND<N>;
+            #[inline]
+            fn div(self, rhs: f64) -> Self::Output {
+                $logic_closure(self, rhs)
+            }
+        }
+    };
+
+    ($lhs:ty, f64) => {
+        impl_div!($lhs, f64, VectorND::generic_alloc_div);
+    };
 }
+
+impl_div!(VectorND<N>, f64, |mut lhs, rhs| { lhs /= rhs; lhs });
+impl_div!(&VectorND<N>, f64);
+impl_div!(&mut VectorND<N>, f64);
