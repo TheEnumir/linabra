d56ac74b28f8d11e02935f4183dd5fe29264cccf
Enumir <zadorin.grigoriy@gmail.com>
2026-09-13 18:34:20 +0300

    Переписал модуль mul для VectorND. Все так же, теперь работает с &mut

--- src/vectornd/operations/mul.rs
+++ src/vectornd/operations/mul.rs
@@ -1,45 +1,47 @@
 use crate::VectorND;
-use std::ops::{Mul, MulAssign};
+use std::{borrow::Borrow, ops::{Mul, MulAssign}};
 
-//Образующие
-impl<const N: usize> Mul<f64> for &VectorND<N> {
-    type Output = VectorND<N>;
+impl<const N: usize> VectorND<N> {
     #[inline]
-    fn mul(self, rhs: f64) -> Self::Output {
-        VectorND::<N>::from_fn(|i| self[i] * rhs)
+    fn generic_alloc_mul(a: impl Borrow<Self>, b: f64) -> Self {
+        let a = a.borrow();
+        Self::from_fn(|i| a[i] * b)
     }
 }
 
 impl<const N: usize> MulAssign<f64> for VectorND<N> {
     #[inline]
     fn mul_assign(&mut self, rhs: f64) {
-        for i in 0..N {
-            self[i] *= rhs
+        for s in self.iter_mut() {
+            *s *= rhs;
         }
     }
 }
 
-//Образованные
-impl<const N: usize> Mul<f64> for VectorND<N> {
-    type Output = Self;
-    #[inline]
-    fn mul(mut self, rhs: f64) -> Self::Output {
-        self *= rhs; self
-    }
+macro_rules! impl_mul {
+    ($lhs:ty, $rhs:ty, $logic_closure:expr) => {
+        impl<const N: usize> Mul<$rhs> for $lhs {
+            type Output = VectorND<N>;
+            #[inline]
+            fn mul(self, rhs: $rhs) -> Self::Output {
+                $logic_closure(self, rhs)
+            }
+        }
+    };
+
+    ($lhs:ty, f64) => {
+        impl_mul!($lhs, f64, VectorND::generic_alloc_mul);
+    };
+
+    (f64, $rhs:ty) => {
+        impl_mul!(f64, $rhs, |lhs, rhs| VectorND::generic_alloc_mul(rhs, lhs));
+    };
 }
 
-impl<const N: usize> Mul<VectorND<N>> for f64 {
-    type Output = VectorND<N>;
-    #[inline]
-    fn mul(self, rhs: VectorND<N>) -> Self::Output {
-        rhs * self
-    }
-}
+impl_mul!(VectorND<N>, f64, |mut lhs, rhs| { lhs *= rhs; lhs });
+impl_mul!(&VectorND<N>, f64);
+impl_mul!(&mut VectorND<N>, f64);
 
-impl<const N: usize> Mul<&VectorND<N>> for f64 {
-    type Output = VectorND<N>;
-    #[inline]
-    fn mul(self, rhs: &VectorND<N>) -> Self::Output {
-        rhs * self
-    }
-}
+impl_mul!(f64, VectorND<N>, |lhs, mut rhs| { rhs *= lhs; rhs });
+impl_mul!(f64, &VectorND<N>);
+impl_mul!(f64, &mut VectorND<N>);
